7ec55dea62d51bb91bcf9dc5ca3e8d3c92568934
三谷朋之 <tomoyuki.mitani@clutch-eng.com>
2025-02-15 12:26:24 +0900

    ユーザテストなど

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,10 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase; // ★ 追加
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase; // ★ 追加
+
 }
